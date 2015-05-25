ad10a8ac72dd3444138db53a18f2660ca8be0656
Radik Fattakhov <radikft@gmail.com>
2015-05-25 22:57:16 +0300

    License

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject textocat-clj-sdk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Unofficial Clojure SDK for Textocat API"
+  :url "http://github.com/radik/textocat-clj-sdk"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
