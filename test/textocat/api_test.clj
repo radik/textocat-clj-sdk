286a7bb3855ef3071fed25e4f4af7be23eaa0085
Radik Fattakhov <radikft@gmail.com>
2015-05-27 00:09:21 +0300

    request-url test extended

--- test/textocat/api_test.clj
+++ test/textocat/api_test.clj
@@ -5,4 +5,6 @@
 (deftest request-url-test
   (testing "Request url forming"
     (is (= (request-url) "http://api.textocat.com"))
-    (is (= (request-url "status") "http://api.textocat.com/status"))))
+    (is (= (request-url "status") "http://api.textocat.com/status"))
+    (is (= (request-url "AUTH_TOKEN" "entity/queue") 
+           "http://api.textocat.com/entity/queue?auth_token=AUTH_TOKEN"))))
