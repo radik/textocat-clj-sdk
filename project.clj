c0e688b30ed79919f9fc9fa3d32ae92ccb621481
Radik Fattakhov <radikft@gmail.com>
2015-05-28 10:31:48 +0300

    clj-http.fake integration, status & entity-queue tests

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.1.2"]
+                 [clj-http-fake "1.0.1"]
                  [cheshire "5.4.0"]])
--- test/textocat/api_test.clj
+++ test/textocat/api_test.clj
@@ -1,6 +1,8 @@
 (ns textocat.api-test
   (:require [clojure.test :refer :all]
-            [textocat.api :refer :all]))
+            [textocat.api :refer :all]
+            [cheshire.core :refer [generate-string]])
+  (:use clj-http.fake))
 
 (deftest request-url-test
   (testing "Request url forming"
@@ -8,3 +10,21 @@
     (is (= (request-url "status") "http://api.textocat.com/status"))
     (is (= (request-url "AUTH_TOKEN" "entity/queue") 
            "http://api.textocat.com/entity/queue?auth_token=AUTH_TOKEN"))))
+
+(deftest status-test
+  (is (= (with-fake-routes
+           {(request-url "status")
+            (fn [request]
+              {:status 200 :headers {} :body (generate-string {:message "The service is operating normally"
+                                                               :statusCode "200 OK"})})}
+           (:statusCode (status))) "200 OK")))
+
+(def auth-token "AUTH_TOKEN")
+
+(deftest entity-queue-test
+  (is (= (with-fake-routes
+           {(request-url auth-token "entity/queue")
+            (fn [request]
+              {:status 200 :headers {} :body (generate-string {:batchId "123"
+                                                               :status "IN_PROGRESS"})})}
+           (:batchId (entity-queue auth-token [{:text "Hello, World!" :tag "test"}]))) "123")))
