f3ea97e099b51d7921c0adb29a8b09fb39c04c14
Radik Fattakhov <radikft@gmail.com>
2015-05-28 22:39:44 +0300

    entity-request test

--- test/textocat/api_test.clj
+++ test/textocat/api_test.clj
@@ -7,9 +7,7 @@
 (deftest request-url-test
   (testing "Request url forming"
     (is (= (request-url) "http://api.textocat.com"))
-    (is (= (request-url "status") "http://api.textocat.com/status"))
-    (is (= (request-url "AUTH_TOKEN" "entity/queue") 
-           "http://api.textocat.com/entity/queue?auth_token=AUTH_TOKEN"))))
+    (is (= (request-url "status") "http://api.textocat.com/status"))))
 
 (deftest status-test
   (is (= (with-fake-routes
@@ -23,8 +21,16 @@
 
 (deftest entity-queue-test
   (is (= (with-fake-routes
-           {(request-url auth-token "entity/queue")
+           {(str (request-url "entity/queue") "?auth_token=" auth-token)
             (fn [request]
               {:status 200 :headers {} :body (generate-string {:batchId "123"
                                                                :status "IN_PROGRESS"})})}
            (:batchId (entity-queue auth-token [{:text "Hello, World!" :tag "test"}]))) "123")))
+
+(deftest entity-request-test
+  (is (= (with-fake-routes
+           {(str (request-url "entity/request") "?auth_token=" auth-token "&batch_id=123")
+            (fn [request]
+              {:status 200 :headers {} :body (generate-string {:batchId "123"
+                                                               :status "FINISHED"})})}
+           (:batchId (entity-request auth-token "123"))) "123")))
