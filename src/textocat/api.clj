c0405cf4e8a0d6f6f5e4cb213e794d544ddbcb12
Radik Fattakhov <radikft@gmail.com>
2015-06-01 10:46:03 +0300

    Documentation

--- src/textocat/api.clj
+++ src/textocat/api.clj
@@ -9,7 +9,10 @@
   ([endpoint] (str base-url "/" endpoint)))
 
 (defn status
-  "Returns a status of the service."
+  "Returns a status of the service.
+  
+  E.g. result {:message \"The service is operating normally\"
+               :statusCode \"200 OK\"}"
   []
   (:body (client/get (request-url "status") {:as :json})))
 
@@ -19,9 +22,13 @@
   Max count of docs is 50. Max size of each document is 50 kB
   (approximatly, each character considered as 2 byte UTF-8 symbol).
 
+  Takes collections of docs, e.g. [{:text \"Hello, World!\", :tag \"clj-test\"}]
+
   The service does not store sources texts.
 
-  Returns collection identity."
+  Returns collection identity. E.g. result
+  {:batchId \"<requested batch id>\", :status \"IN_PROGRESS\"}
+  "
   [auth-token docs]
   (:body (client/post (request-url "entity/queue") 
                       {:query-params {"auth_token" auth-token}
@@ -30,7 +37,12 @@
                        :as :json})))
 
 (defn entity-request
-  ""
+  "Returns status of collection by specified batch id.
+
+  Returns {:batchId \"<requested batch id>\", :status \"IN_PROGRESS\"}
+  if collection is still processing or
+  {:batchId \"<requested batch id>\", :status \"FINISHED\"}
+  if collection processing finished."
   [auth-token batch-id]
   (:body (client/get (request-url "entity/request") 
                      {:query-params {"auth_token" auth-token
@@ -38,7 +50,22 @@
                       :as :json})))
 
 (defn entity-retrieve
-  ""
+  "Returns array of documents with recognized entities by batch ids.
+
+  It's possible to retrieve up to 100 collections of documents.
+  E.g. result {:batchIds [\"1\",\"2\"]
+               :documents [{:status \"FINISHED\"
+                            :tag \"clj-test\"
+                            :entities [{:span \"World\"
+                                        :beginOffset 7
+                                        :endOffset 12
+                                        :category \"LOCATION\"}]}
+                           {:status \"FINISHED\"
+                            :tag \"clj-test\"
+                            :entities [{:span \"Мир\"
+                                        :beginOffset 8
+                                        :endOffset 11
+                                        :category \"LOCATION\"}]}]}"
   [auth-token & batch-ids]
   (:body (client/get (request-url "entity/retrieve")
                      {:query-params {"auth_token" auth-token
@@ -46,7 +73,19 @@
                       :as :json})))
 
 (defn entity-search
-  ""
+  "Searching in all documents uploaded by user.
+
+  Documents are sorted by relevance. Exteneded syntax for search 
+  queries are supported. For more information about query syntax
+  look at http://docs.textocat.com/search-query-syntax.pdf.
+
+  E.g. result {:searchQuery \"LOCATION:World\"
+               :documents [{:status \"FINISHED\"
+                            :tag \"clj-test\"
+                            :entities [{:span \"World\"
+                                        :beginOffset 7
+                                        :endOffset 12
+                                        :category \"LOCATION\"}]}]}"
   [auth-token search-query]
   (:body (client/get (request-url "entity/search")
                      {:query-params {"auth_token" auth-token
